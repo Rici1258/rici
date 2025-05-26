022a054623da01e9736de869bb1f6b52da375fcc
Rici1258 <richard.varga@student.ssnd.sk>
2025-05-26 11:17:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import hashlib
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(name, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True)
 
 db_path = os.path.abspath("kurzy.db")
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}".replace
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}".replace("\", "/")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
@@ -180,7 +180,7 @@ def pridaj_kurz():
         <a href="/"><button type="button">Späť</button></a>
     '''
 
-if name == "main":
+if __name__ == "main":
     os.makedirs(app.instance_path, exist_ok=True)
     with app.app_context():
         db.create_all()
